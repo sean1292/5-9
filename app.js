bc688af71f707fadc993c7f56365a13a9f17fc3d
sean1292 <sean.sadykoff@gmail.com>
2020-05-06 14:57:53 -0700

    added basic cookie check

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const { v4: uuid } = require('uuid');
+
 const app = express();
+
 app.use(cookieParser());
 app.use(express.static('public'));
-app.get('/', (req, res) => {
-  console.log(req.cookies);
-  res.send('hello');
+
+const users = {};
+const cookies = {};
+app.get('/cookie', (req, res) => {
+  const cookie = req.cookies.userCookie;
+  if (!cookie) {
+    res.sendStatus(404);
+  } else {
+    res.status(200).json({ username: cookies[cookie] });
+  }
 });
 
 app.listen(process.env.PORT || 8123);
