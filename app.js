95fea89ac0800e3c0f5459e775bf15bcfb92adf9
sean1292 <sean.sadykoff@gmail.com>
2020-05-05 19:03:01 -0700

    added cookie parser

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
+app.use(cookieParser());
 app.use(express.static('public'));
 app.get('/', (req, res) => {
+  console.log(req.cookies);
   res.send('hello');
 });
 
